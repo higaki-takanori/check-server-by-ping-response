5788a9d7fe7f120c70140fab494299c9682850e1
higaki-takanori <garoumare@gmail.com>
2021-12-05 16:56:21 +0900

    [STEP1]故障状態のサーバを検出する関数を作成

--- check_server_status.py
+++ check_server_status.py
@@ -5,11 +5,18 @@ LOGFILENAME = "ping.log"
 if __name__ == '__main__':
   logs = LogCollection()
   for line in open(LOGFILENAME, "r"):
-    datetime, ipaddress, restime = line.split("¥n")[0].split(",")
-    # print(datetime, ipaddress, restime)
+    datetime, ipaddress, restime = line.split("\n")[0].split(",")
     logs.append(Log(datetime, ipaddress, restime))
 
-  print(logs.__class__)
-  print(logs)
-  for log in logs:
-    print(log.address())
+  errors = logs.get_errors()
+  print(errors)
+  for error, value in errors.items():
+    print(error, value)
+
+  print(logs.get_errors())
+
+  # for log in logs:
+  #   print(log.datetime - time)
+  #   print(time)
+  #   time = log.datetime
+  #   print(log.datetime, log.ipaddress, log.restime)
--- classes/log_class.py
+++ classes/log_class.py
@@ -1,23 +1,33 @@
+from datetime import datetime as dt
+
+TIMESTR = "%Y%m%d%H%M%S"
+
 class Log:
   def __init__(self, datetime, ipaddress, restime):
-    self.datetime = datetime
+    self.datetime = dt.strptime(datetime, TIMESTR)
     self.ipaddress = ipaddress
     self.restime = restime
 
-  def is_arrive(self):
+  def is_timeout(self):
     if self.restime == '-':
-      return -1
+      return True
     else:
-      return 0
+      return False
 
-  def subnet(self):
+  def get_subnet(self):
     subnet = self.ipaddress.split('/')[1]
     return subnet
 
-  def address(self):
+  def get_address(self):
     address = self.ipaddress.split('/')[0]
     return address
 
 class LogCollection(list):
-  def __init__(self, logs=[]):
-    self.logs = logs
+  def __init__(self):
+    self.errors = {}
+
+  def get_errors(self):
+    for log in self:
+      if log.is_timeout():
+        self.errors[log.ipaddress] = log.datetime
+    return self.errors
